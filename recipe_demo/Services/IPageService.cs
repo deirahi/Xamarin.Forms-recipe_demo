3dbad6b316296836c46fd4fbbca41a669caf005a
deirahi <hiraide.atkuya.dash@gmail.com>
2020-10-12 08:59:12 +0900

    pageservice,IPageServiceの追加
    pageのpushとpop、簡易的なダイアログの表示を実装

---
+++ recipe_demo/Services/IPageService.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace recipe_demo.Services
+{
+    public interface IPageService
+    {
+        Task PushAsync(Page page);
+        Task<Page> PopAsync();
+
+        Task<bool> DisplayAlert(string title, string message, string ok, string cansel);
+        Task DisplayAlert(string title, string message, string ok);
+    }
+}
---
+++ recipe_demo/Services/PageService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace recipe_demo.Services
+{
+    public class PageService : IPageService
+    {
+        private Page mainPage
+        {
+            get { return Application.Current.MainPage; }
+        }
+
+
+        public async Task<Page> PopAsync()
+        {
+            return await mainPage.Navigation.PopAsync();
+        }
+
+        public async Task PushAsync(Page page)
+        {
+            await mainPage.Navigation.PushAsync(page);
+        }
+
+        public async Task<bool> DisplayAlert(string title, string message, string ok, string cansel)
+        {
+            return await mainPage.DisplayAlert(title, message, ok, cansel);
+        }
+
+        public async Task DisplayAlert(string title, string message, string ok)
+        {
+            await mainPage.DisplayAlert(title, message, ok);
+        }
+    }
+}
