0e2213f10a4a1651b6744ac189599420af637271
deirahi <hiraide.atkuya.dash@gmail.com>
2020-10-13 21:38:21 +0900

    BaseViewModelを作成 INotifyPropertyChangedを実装し、Propertyの変更がわかるようにした
    MessagingCenter.Subscribeで使うMassage用のstaticクラスを作成

---
+++ recipe_demo/Services/DbChangeEventMassages.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace recipe_demo.Services
+{
+    public static class DbChangeEventMassages
+    {
+        public static string RecipeAdded = "AddRecipe";
+        public static string RecipeUpdated = "UpdateRecipe";
+    }
+}
